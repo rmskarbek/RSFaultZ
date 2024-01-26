26472968d34d6a9fd49b1baa01b21eeb3815a725
Rob <rob@pop-os.localdomain>
2024-01-26 14:16:20 -0500

    clean up the code

--- app/StrikeSlipStress_HODLR.m
+++ app/StrikeSlipStress_HODLR.m
@@ -1,42 +1,37 @@
 function Kernel = StrikeSlipStress_HODLR(i, j, xi, beta)
-%%% This is in my coordinates.
-I = sqrt(-1);
+
 %%% Along-fault spatial coordinate.
 beta = (pi/180)*beta;
-%theta = 0;
-%theta = pi + alpha*pi/180;
 theta = beta - pi/2;
+z = xi(i)'*exp(1i*beta);
 
 %%% Variables s are the integrands
 dxi = mean(diff(xi));
 
 %%% Build discretized elastic stress operator assuming piecewise continuous
-%%% slip distribution.             
+%%% slip distribution.
+%%% Edge 1.
 s = xi(j) - dxi/2;
-
-z = xi(i)'*exp(I*beta);
-%   zbar = Xi(i)*exp(-I*theta);
-z_0 = s*exp(I*beta);
-zbar_0 = s*exp(-I*beta);
-
-%%% Potential.
-Y = exp(I*theta).*(1./(z - z_0) - 1./(z - zbar_0));
-
-%%% Shear stress due to screw dislocation
-Shear_1 = imag(Y);
-
+Shear_1 = StrikeSlipPotentials(s, z, beta, theta);
 
 %%% Edge 2.
 s = xi(j) + dxi/2;
-
-z_0 = s*exp(I*beta);
-zbar_0 = s*exp(-I*beta);
-
-%%% Potentials.
-Y = exp(I*theta).*(1./(z - z_0) - 1./(z - zbar_0));
-
-%%% Shear stress due to screw dislocation
-Shear_2 = imag(Y);
+Shear_2 = StrikeSlipPotentials(s, z, beta, theta);
 
 %%% Assemble.
 Kernel =  Shear_1 - Shear_2;
+
+    function Shear = StrikeSlipPotentials(s, z, beta, theta)
+    
+    %%% z_0 is the location of the dislocation. I.E. on-fault coordinate.
+        z_0 = s*exp(1i*beta);
+        zbar_0 = conj(z_0);
+    
+    %%% Potential.    
+        Y = exp(1i*theta).*(1./(z - z_0) - 1./(z - zbar_0));
+    
+    %%% Shear stress due to shear dislocation
+        Shear = imag(Y);
+    end
+
+end
