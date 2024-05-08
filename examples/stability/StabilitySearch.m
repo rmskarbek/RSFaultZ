443bed70349dd82a17280d325ed3e33bc97b3e38
Rob <rob@pop-os.localdomain>
2024-05-08 14:49:12 -0400

    set tolerance to grid spacing

--- examples/stability/StabilitySearch.m
+++ examples/stability/StabilitySearch.m
@@ -1,4 +1,4 @@
-function [R, p, L_star] = StabilitySearch(ab, beta, mu_0, geom, Burial_Star, tol, calc, R)
+function [R, p, L_hat, tol] = StabilitySearch(ab, beta, mu_0, geom, Burial_hat, calc, type, R)
 
 %%% For a given set of RSF and elastic parameters, this function carries out a search
 %%% for the critical fault length where the stability changes. This function 
@@ -13,7 +13,25 @@ function [R, p, L_star] = StabilitySearch(ab, beta, mu_0, geom, Burial_Star, tol
     stab_a = 1;     % System is stable
     % stab_b = 0;     % System is unstable
 
-%%% Maximum number of steps.    
+%%% The smallest allowable search interval is the grid spacing. So set up a grid to get this
+%%% value. Use a dummy value for L_hat.
+    L_hat1 = 1;
+    if exist('R', 'var')
+        [R, p] = StabilitySetup(ab, L_hat1, beta, mu_0, geom, Burial_hat, type, R);
+    else
+        [R, p] = StabilitySetup(ab, L_hat1, beta, mu_0, geom, Burial_hat, type);
+    end
+%%% Search tolerance is defined as 1/2 of the search interval. Account for the different
+%%% scaling for thin layer geometry.
+    if strcmp(geom, 'Layer') == 1
+        d = Burial_hat*p.Friction.L_b;
+        H_s = (8*pi*d*p.Friction.H_star)^(1/2);
+        tol = 0.5*(p.Geometry.GridSpacing/H_s);
+    else
+        tol = 0.5*(p.Geometry.GridSpacing/p.Friction.H_star);
+    end        
+
+%%% Maximum number of steps.
     q_max = 100;
     q = 1;
     while q < q_max
@@ -25,14 +43,10 @@ function [R, p, L_star] = StabilitySearch(ab, beta, mu_0, geom, Burial_Star, tol
         end
 
     %%% Check the stability at the midpoint of current interval.
-        L_star = (a + b)/2;
+        L_hat = (a + b)/2;
    
     %%% Set up the model.
-        if exist('R', 'var')
-            [R, p] = StabilitySetup(ab, L_star, beta, mu_0, geom, Burial_Star, R);
-        else
-            [R, p] = StabilitySetup(ab, L_star, beta, mu_0, geom, Burial_Star);
-        end
+        [R, p] = StabilitySetup(ab, L_hat, beta, mu_0, geom, Burial_hat, type, R);
     
     %%% Conduct the stability calculations.
         Out_S = StabilityAnalysis(p, calc, geom);
@@ -40,10 +54,10 @@ function [R, p, L_star] = StabilitySearch(ab, beta, mu_0, geom, Burial_Star, tol
 
     %%% Update the search interval.       
         if stab == stab_a
-            a = L_star;
+            a = L_hat;
             % stab_a = stab;
         else
-            b = L_star;
+            b = L_hat;
         end
 
     %%% Increment the step counter.
