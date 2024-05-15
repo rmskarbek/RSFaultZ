89914be9ef3e54c7450e29bcd1d0447fee5e52f8
Rob <rob@pop-os.localdomain>
2024-05-15 13:41:58 -0400

    added 5 km plots

--- benchmarks/BP6QD/BP6QD_Plots.m
+++ benchmarks/BP6QD/BP6QD_Plots.m
@@ -22,6 +22,8 @@ spy = 365.25*24*3600;
 
 %%% On fault data from Dublanchet at 0 km.
 C = readtable('Dublanchet_fltst_strk_00.txt');
+C5 = readtable('Dublanchet_fltst_strk_05.txt');
+% C = readtable('Ozawa_fltst_strk_00b.txt');
 
 %%%--------------------------------------------------------------------------------------%%%
 %%% Slip velocity.
@@ -30,14 +32,17 @@ subplot(3, 1, 1)
 plot(OnFaultData.fltst_strk_p0.Time_s/spy,  OnFaultData.fltst_strk_p0.SlipRate_log10ms, 'k',...
     'LineWidth', 2)
 hold on
+plot(OnFaultData.fltst_strk_p5.Time_s/spy,  OnFaultData.fltst_strk_p5.SlipRate_log10ms, 'k',...
+    'LineWidth', 2)
 plot(C.t/spy, C.slip_rate, 'm--', 'LineWidth', 2);
+plot(C5.t/spy, C5.slip_rate, 'b--', 'LineWidth', 2);
 hold off
 
 xlim([0 2])
 ylim([-12 -6])
 xlabel('Time (years)')
 ylabel('log[Slip Rate (m/s)]')
-legend('RSFaultZ', 'Dublanchet', 'Location', 'northeast');
+legend('RSFaultZ', '', 'Dublanchet 0 km', 'Dublanchet +5 km', 'Location', 'northeast');
 
 %%%--------------------------------------------------------------------------------------%%%
 %%% Shear stress.
@@ -45,14 +50,17 @@ subplot(3, 1, 2)
 plot(OnFaultData.fltst_strk_p0.Time_s/spy, OnFaultData.fltst_strk_p0.ShearStress_MPa, 'k',...
     'LineWidth', 2)
 hold on
+plot(OnFaultData.fltst_strk_p5.Time_s/spy, OnFaultData.fltst_strk_p5.ShearStress_MPa, 'k',...
+    'LineWidth', 2)
 plot(C.t/spy, C.shear_stress, 'm--', 'LineWidth', 2);
+plot(C5.t/spy, C5.shear_stress, 'b--', 'LineWidth', 2);
 hold off
 
 xlim([0 2])
-ylim([25 30])
+ylim([25 31])
 xlabel('Time (years)')
 ylabel('Shear Stress (MPa)')
-legend('RSFaultZ', 'Dublanchet', 'Location', 'northeast');
+legend('RSFaultZ', '', 'Dublanchet 0 km', 'Dublanchet +5 km', 'Location', 'northeast');
 
 %%%--------------------------------------------------------------------------------------%%%
 %%% Pore pressure.
@@ -60,11 +68,14 @@ subplot(3, 1, 3)
 plot(OnFaultData.fltst_strk_p0.Time_s/spy, OnFaultData.fltst_strk_p0.PorePressure_MPa, 'k',...
     'LineWidth', 2)
 hold on
+plot(OnFaultData.fltst_strk_p5.Time_s/spy, OnFaultData.fltst_strk_p5.PorePressure_MPa, 'k',...
+    'LineWidth', 2)
 plot(C.t/spy, C.pore_pressure, 'm--', 'LineWidth', 2);
+plot(C5.t/spy, C5.pore_pressure, 'm--', 'LineWidth', 2);
 hold off
 
 xlim([0 2])
 ylim([0 8])
 xlabel('Time (years)')
 ylabel('Pore Pressure (MPa)')
-legend('RSFaultZ', 'Dublanchet', 'Location', 'northeast');
+legend('RSFaultZ', '', 'Dublanchet 0 km', 'Dublanchet +5 km', 'Location', 'northeast');
