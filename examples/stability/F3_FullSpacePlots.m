ea2920e57e8dfd592e71be038d3499c4b8411cb1
Rob Skarbek <rskarbek@psi.edu>
2025-02-19 08:36:31 -0500

    Updated for loading included .mat files.

--- examples/stability/F3_FullSpacePlots.m
+++ examples/stability/F3_FullSpacePlots.m
@@ -18,12 +18,12 @@
 %%%     2a. SimulationSetup.m: Creates an instance of RSFaultZ; sets the
 %%%     parameter values; and runs the simulation.
 
-%%% 3. Generates Figure 3 in Skarbek, 2024 JGR.
-
 %%%---------------------------------------------------------------------%%%
 
 %%% This script takes ~21 minutes to execute on Rob's computer: 3.7 GHz 
 %%% processor, 64 GB RAM, Linux OS.
+
+%%% Edge set with EffStress_0 = 5 MPa took 57 min.
 
 %%%---------------------------------------------------------------------%%%
 %%%---------------------------------------------------------------------%%%
@@ -40,83 +40,123 @@ beta = nan;
 mu_0 = 0.6;
 
 %%% Values of a/b.
-ab1 = [0.7; 0.9];
+ab1 = [0.3; 0.5; 0.7];
 
 %%% Full space. Values of fault length as Length_star = FaultLength/h_star_F.
 %%% Note: exp(-1) = 0.3679. 0.36 might be unstable on long enough
 %%% timescales; but 0.35 decays, so is definitely stable.
 calc = 'length';
-Length_starL = [0.35; 0.36; exp(-1); 0.37; 0.38; 0.39; (0.4:0.1:1)'];
+Length_hat = [0.35; 0.36; exp(-1); 0.37; 0.3725; 0.375; 0.38; 0.39; 0.4];
+
+%%%---------------------------------------------------------------------%%%
+%%%---------------------------------------------------------------------%%%
+%%% To re-run the simulations, comment out the load commands on Lines 55
+%%% and 56, and uncomment Lines 59 - 61 and 65 - 67.
+load('StabilityData_Fig3.mat')
+load('StabilityData_Fig3_Edge.mat')
 
 %%% First run with 5 MPa effective stress.
-EffStress_0 = 5;
-[T_L5, P_L5] = SimulationRun1(ab1, Length_starL, geom, beta, mu_0, calc,...
-    Burial_Star, EffStress_0);
+% EffStress_0 = 5;
+% [T_L5, P_L5] = SimulationRun1(ab1, Length_hat, geom, beta, mu_0, calc,...
+%     Burial_Star, EffStress_0);
+
 
 %%% Second run with 50 MPa effective stress.
-EffStress_0 = 50;
-[T_L50, P_L50] = SimulationRun1(ab1, Length_starL, geom, beta, mu_0, calc,...
-    Burial_Star, EffStress_0);
+% EffStress_0 = 50;
+% [T_L50, P_L50] = SimulationRun1(ab1, Length_hat, geom, beta, mu_0, calc,...
+%     Burial_Star, EffStress_0);
 
+%%%---------------------------------------------------------------------%%%
+%%%---------------------------------------------------------------------%%%
 %%% This loop pulls out the maximum slip speeds for each value of
 %%% Length_starF.
-V_maxF5 = nan(numel(Length_starL), numel(ab1));
-V_maxF50 = nan(numel(Length_starL), numel(ab1));
+V_maxF5 = nan(numel(Length_hat), numel(ab1));
+V_maxF50 = nan(numel(Length_hat), numel(ab1));
 
-for i = 1:numel(Length_starL)
+for i = 1:numel(Length_hat)
     for j = 1:numel(ab1)
         V_maxF5(i, j) = max(T_L5.(i){j,1}.Vmax);
-        V_maxF50(i, j) = max(T_L50.(i){j,1}.Vmax);
+        V_maxF50(i, j) = max(T_L50.(i){j,1}.Vmax);   
     end
 end
 
 %%% Get the plate rate, it's the same for each simulation.
 v_plate = P_L5.(1){1,1}.Material.PlateRate;
 
+%%%---------------------------------------------------------------------%%%
+%%% Review results. Uncomment to view time series plots of the maximum slip velocity
+%%% for each simulation.
+% spy = 365.25*24*3600;
+% for j = 1:numel(ab1)
+%     for i = 1:numel(Length_hat)
+%         f = figure;
+%         semilogy(T_L5.(i){j,1}.Time/spy, T_L5.(i){j,1}.Vmax, 'r')
+%         title(sprintf('a/b = %.1f, Lhat = %.4f', [ab1(j), Length_hat(i)]))
+%         pause
+%         close(f)
+%     end
+% end
+% 
+% for j = 1:numel(ab1)
+%     for i = 1:numel(Length_hat)
+%         f = figure;
+%         semilogy(T_L50.(i){j,1}.Time/spy, T_L50.(i){j,1}.Vmax, 'r')
+%         title(sprintf('a/b = %.1f, Lhat = %.4f', [ab1(j), Length_hat(i)]))
+%         pause
+%         close(f)
+%     end
+% end
 
 %%%---------------------------------------------------------------------%%%
 %%%---------------------------------------------------------------------%%%
-%%% Plot results. MarkerHandle.LineWidth sets the marker border line
-%%% thickness separately from the 'LineWidth' setting.
-figure;
-
-%%% (a/b) = 0.7, L_b = 0.8 km
-p3 = plot(Length_starL, V_maxF50(:,1)/v_plate, 'c--s', 'MarkerEdgeColor', 'k',...
-    'MarkerFaceColor', 'c', 'MarkerSize', 15, 'LineWidth', 3);
-drawnow
-p3.MarkerHandle.LineWidth = 1;
-hold on;
-
-%%% (a/b) = 0.9, L_b = 0.8 km
-p4 = plot(Length_starL, V_maxF50(:,2)/v_plate, 'm--s', 'MarkerEdgeColor', 'k',...
-    'MarkerFaceColor', 'm', 'MarkerSize', 10, 'LineWidth', 3);
-drawnow
-p4.MarkerHandle.LineWidth = 1;
-
-%%% (a/b) = 0.7, L_b = 8 km
-p1 = plot(Length_starL, V_maxF5(:,1)/v_plate, 'c-s', 'MarkerEdgeColor', 'k',...
-    'MarkerFaceColor', 'c', 'MarkerSize', 15, 'LineWidth', 3);
-drawnow
-p1.MarkerHandle.LineWidth = 1;
-
-%%% (a/b) = 0.9, L_b = 8 km
-p2 = plot(Length_starL, V_maxF5(:,2)/v_plate, 'm-s', 'MarkerEdgeColor', 'k',...
-    'MarkerFaceColor', 'm', 'MarkerSize', 10, 'LineWidth', 3);
-drawnow
-p2.MarkerHandle.LineWidth = 1;
+%%% Plot results.
+figure
+set(gcf,'Color','w')
+hold on
+box on
+cmap = crameri('batlow');
+colors = [cmap(256/4,:); cmap(256/2,:); cmap(3*256/4,:)];
+P = [{'p_3_5'; 'p_5_5'; 'p_7_5'}, {'p_3_50'; 'p_5_50'; 'p_7_50'}];
 
 
-plot(exp(-1)*[1 1], [1 1e9], 'k--', 'LineWidth', 3)
+for i = 1:numel(ab1)
+    eval([P{i,1}, '= plot(Length_hat, V_maxF5(:,i)/v_plate, ''s-'', ''Color'', colors(i,:), ''MarkerFaceColor'', colors(i,:), ''MarkerEdgeColor'', ''k'', ''MarkerSize'', 20, ''LineWidth'', 3);']);
+    drawnow
+    eval([P{i,1},'.MarkerHandle.LineWidth = 1'])
+    % plot(Length_starL, V_maxF5(:,i)/v_plate, 's-', 'Color', colors{i}, 'MarkerFaceColor',...
+    %     colors{i}, 'MarkerEdgeColor', 'k', 'MarkerSize', 15)
+end
+
+for i = 1:numel(ab1)
+    eval([P{i,2}, '= plot(Length_hat, V_maxF50(:,i)/v_plate, ''d-'', ''Color'', colors(i,:), ''MarkerFaceColor'', colors(i,:), ''MarkerEdgeColor'', ''k'', ''MarkerSize'', 15, ''LineWidth'', 3);']);
+    drawnow
+    eval([P{i,2},'.MarkerHandle.LineWidth = 1'])
+    % plot(Length_starL, V_maxF50(:,i)/v_plate, 'd-', 'Color', colors{i}, 'MarkerFaceColor',...
+    %     colors{i}, 'MarkerEdgeColor', 'k', 'MarkerSize', 10)
+end
+plot(exp(-1)*[1 1], [9e-1 2e2], 'k--', 'LineWidth', 3)
+
+%%% Mark locations of the critical fault lengths.
+plot(mean(Length_hat(4:5)), 10^mean(log10([V_maxF5(4,1), V_maxF5(5,1)]))/v_plate, 'x',...
+    'Color', colors(1,:), 'LineWidth', 3, 'MarkerSize', 15)
+plot(mean(Length_hat(5:6)), 10^mean(log10([V_maxF5(5,2), V_maxF5(6,2)]))/v_plate, 'x',...
+    'Color', colors(2,:), 'LineWidth', 3, 'MarkerSize', 15)
+plot(mean(Length_hat(4:5)), 10^mean(log10([V_maxF5(4,3), V_maxF5(5,3)]))/v_plate, 'x',...
+    'Color', colors(3,:), 'LineWidth', 3, 'MarkerSize', 15)
+
+%%% Plot the edge perturbation results.
+plot(Length_hat, V_maxF5_Edge(:,1)/v_plate, 'ko', 'MarkerSize', 8)
+plot(Length_hat, V_maxF5_Edge(:,2)/v_plate, 'ko', 'MarkerSize', 8)
+plot(Length_hat, V_maxF5_Edge(:,3)/v_plate, 'ko', 'MarkerSize', 8)
+
 hold off
-
-xlim([0.3 1])
-ylim([1 1e9])
-
+xlim([0.349 0.401])
+ylim([9e-1 2e2])
 ax = gca;
-ax.XTick = [0.3, exp(-1), (0.4:0.1:1)];
-ax.XTickLabel{2,1} = '$e^{-1}$';
 ax.YScale = 'log';
-ax.YTick = logspace(0,9,10);
+ax.XTick = (0.35:0.01:0.4);
+% ax.XTick = [0.35, 0.36, exp(-1), (0.37:0.01:0.4)];
+% ax.XTickLabel{3,1} = '$e^{-1}$';
 ax.FontSize = 20;
 ax.TickLabelInterpreter = 'latex';
 
@@ -126,45 +166,20 @@ ylabel('Maximum Slip Velocity ($v / v_0$)', 'Interpreter', 'latex')
 %%% Add the legend. For tabular legends see:
 %%% https://www.mathworks.com/matlabcentral/answers/519942-creating-a-tabular-legend
 %%% https://www.mathworks.com/matlabcentral/answers/339686-how-do-i-get-a-tabular-legend
-lgd = legend('{ }', '{ }', '\ \ \ 0.7', '\ \ \ 0.9', 'Location', 'northwest',...
-    'Interpreter', 'latex');
+lgd = legend('{ }', '{ }', '{ }', '\ \ \ 0.3', '\ \ \ 0.5', '\ \ \ 0.7', 'Location',...
+    'northwest', 'Interpreter', 'latex');
 lgd.NumColumns = 2;
 
 title(lgd, ['$L_b$ (km) $\ \ \ \ \ \ $' newline '\ 0.8 \ \ \ \  8 $\ \ \ (a/b)$'],...
     'Interpreter', 'latex')
 drawnow
 
-%%% Remove the markers from the legend entries.
-lineEntry = findobj(lgd.EntryContainer, 'Object',p1);
-entryMarker = findobj(lineEntry.Icon.Transform, 'Description','Icon Marker');
-entryMarker.Style = 'none';
-
-lineEntry = findobj(lgd.EntryContainer, 'Object',p2);
-entryMarker = findobj(lineEntry.Icon.Transform, 'Description','Icon Marker');
-entryMarker.Style = 'none';
-
-lineEntry = findobj(lgd.EntryContainer, 'Object',p3);
-entryMarker = findobj(lineEntry.Icon.Transform, 'Description','Icon Marker');
-entryMarker.Style = 'none';
-
-lineEntry = findobj(lgd.EntryContainer, 'Object',p4);
-entryMarker = findobj(lineEntry.Icon.Transform, 'Description','Icon Marker');
-entryMarker.Style = 'none';
-
-% %%%---------------------------------------------------------------------%%%
-% %%%---------------------------------------------------------------------%%%
-% 
-% %%% Calculation 2: Run full space simulations for different perturation 
-% %%% lengths. 
-% calc = 'perturbation';
-% Length_starP = 1./2.^(6:-1:0)';
-% [T_P, P_P] = SimulationRun1(ab1(1), Length_starP, geom, beta, mu_0, calc,...
-%     Burial_Star);
-% 
-% %%% This loop pulls out the maximum slip speeds for each value of
-% %%% Length_starP.
-% V_maxP = nan(numel(Length_starP),1);
-% 
-% for i = 1:numel(Length_starP)
-%     V_maxP(i, 1) = max(T_P.(i){1,1}.Vmax);
-% end
+%%% Change the line width of the legend markers.
+for i = 1:3
+    for j = 1:2
+        lineEntry = findobj(lgd.EntryContainer, 'Object', eval(P{i,j}));
+        entryMarker = findobj(lineEntry.Icon.Transform, 'Description','Icon Marker');
+        entryMarker.LineWidth = 1.0;
+        % entryMarker.Style = 'none';
+    end
+end
--- examples/stability/F45_HalfSpaceStability.m
+++ examples/stability/F45_HalfSpaceStability.m
@@ -19,7 +19,6 @@
 %%%     2b. StabilityAnalysis.m: Executes the numerical stability analysis
 %%%     using the parameters structure that is created by StabilitySetup.m
 
-%%% 3. Generates Figures 4 and 5A in Skarbek, 2024 JGR.
 
 %%%---------------------------------------------------------------------%%%
 
@@ -29,11 +28,13 @@
 
 %%%---------------------------------------------------------------------%%%
 
-%%% This script takes ~25 minutes to execute on Rob's computer: 3.7 GHz 
+%%% This script takes ~3.3 hours to execute on Rob's computer: 3.7 GHz 
 %%% processor, 64 GB RAM, Linux OS.
 
-%%% dxi =  H_s_F/250 in StabilitySetup.m, which gives min(dxi) ~ L_d/40. 
 %%% In total, 4700 individual numerical stability calculations are executed.
+%%% The long computation time is due to this, and the small grid spacing:
+%%% dxi = H_s_F/250 or dxi = L_b/80, whichever is smaller.
+
 
 %%%---------------------------------------------------------------------%%%
 %%%---------------------------------------------------------------------%%%
@@ -43,59 +44,66 @@
 %%% Set the geometry for these calculations.
 geom = [{'Thrust Fault'}; {'Normal Fault'}];
 calc = 'boundary';
+type = 'search';
 
 %%% Values of fault dip and normalized burial depth for the calculations.
 beta = (0:0.5:90)';
 % beta = [0 0.5 10 90]';
-Burial_Star = [0; 0.001; 0.01; 0.1; 1];
+Burial_hat = [0; 0.001; 0.01; 0.1; 1];
 
 %%% Values of a/b.
 ab = 0.8;
 
-%%% StabilityRun1 does not use the value of Length_star for 'Thrust Fault'
+%%% StabilityRun1 does not use the value of Length_hat for 'Thrust Fault'
 %%% and 'Normal Fault' geometries.
-Length_star = 5; % dummy value.
+Length_hat = nan; % dummy value.
+
+%%%---------------------------------------------------------------------%%%
+%%%---------------------------------------------------------------------%%%
+%%% To re-run the simulations, comment out the load command on Lines 65
+%%% and uncomment Lines 68 - 93.
+load('StabilityData_Fig4.mat')
 
 %%% Thrust faults.
-j = 1;
-mu_0 = 0.6;
-[T_T06, P_T06] = StabilityRun1(ab, Length_star, calc, geom{j}, beta, mu_0,...
-    Burial_Star);
+% j = 1;
+% mu_0 = 0.6;
+% [T_T06, P_T06] = StabilityRun1(ab, Length_hat, calc, geom{j}, beta, mu_0,...
+%     Burial_hat, type);
+% 
+% mu_0 = 0.2;
+% [T_T02, P_T02] = StabilityRun1(ab, Length_hat, calc, geom{j}, beta, mu_0,...
+%     Burial_hat(1:4), type);
+% 
+% mu_0 = 1;
+% [T_T1, P_T1] = StabilityRun1(ab, Length_hat, calc, geom{j}, beta, mu_0,...
+%     Burial_hat(1:4), type);
+% 
+% %%% Normal faults.
+% j = 2;
+% mu_0 = 0.6;
+% [T_N06, P_N06] = StabilityRun1(ab, Length_hat, calc, geom{j}, beta, mu_0,...
+%     Burial_hat, type);
+% 
+% mu_0 = 0.2;
+% [T_N02, P_N02] = StabilityRun1(ab, Length_hat, calc, geom{j}, beta, mu_0,...
+%     Burial_hat(1:4), type);
+% 
+% mu_0 = 1;
+% [T_N1, P_N1] = StabilityRun1(ab, Length_hat, calc, geom{j}, beta, mu_0,...
+%     Burial_hat(1:4), type);
 
-mu_0 = 0.2;
-[T_T02, P_T02] = StabilityRun1(ab, Length_star, calc, geom{j}, beta,...
-    mu_0, Burial_Star(1:4));
-
-mu_0 = 1;
-[T_T1, P_T1] = StabilityRun1(ab, Length_star, calc, geom{j}, beta,...
-    mu_0, Burial_Star(1:4));
-
-%%% Normal faults.
-j = 2;
-mu_0 = 0.6;
-[T_N06, P_N06] = StabilityRun1(ab, Length_star, calc, geom{j}, beta, mu_0,...
-    Burial_Star);
-
-mu_0 = 0.2;
-[T_N02, P_N02] = StabilityRun1(ab, Length_star, calc, geom{j}, beta,...
-    mu_0, Burial_Star(1:4));
-
-mu_0 = 1;
-[T_N1, P_N1] = StabilityRun1(ab, Length_star, calc, geom{j}, beta,...
-    mu_0, Burial_Star(1:4));
-
-
-%%%---------------------------------------------------------------------%%%
-%%%---------------------------------------------------------------------%%%
+%%%-------------------------------------------------------------------------------%%%
+%%%-------------------------------------------------------------------------------%%%
 figure;
-tiledlayout(1,2);
+set(gcf,'Color','w')
+layout1 = tiledlayout(1, 2, 'Padding', 'tight');
 col = {'m'; 'b'; 'c'; 'g'; 'k'};
-nexttile
+nexttile(layout1)
 hold on;
 box on;
 
 %%% Plot thrust fault results.
-for i = 1:numel(Burial_Star)
+for i = 1:numel(Burial_hat)
     if i == 1
         plot(beta(2:end), cell2mat(T_T06.(i)), col{i}, 'LineWidth', 3);
     else
@@ -103,7 +111,7 @@ for i = 1:numel(Burial_Star)
     end
 end
 
-for i = 1:numel(Burial_Star(1:4))
+for i = 1:numel(Burial_hat(1:4))
     if i == 1
         plot(beta(2:end), cell2mat(T_T02.(i)), col{i}, 'LineWidth', 3,...
             'LineStyle', '--');
@@ -128,7 +136,7 @@ ylabel('Critical Fault Length ($L^*_D / h_F^*$)', 'Interpreter', 'latex')
 xlim([0 90])
 ylim([0 0.4])
 ax = gca;
-ax.FontSize = 20;
+ax.FontSize = 22;
 ax.XTick = 0:10:90;
 ax.YTick = [(0:0.05:0.35) exp(-1) 0.4];
 ax.YTickLabel{9,1} = '$e^{-1}$';
@@ -136,10 +144,10 @@ ax.TickLabelInterpreter = 'latex';
 
 %%%---------------------------------------------------------------------%%%
 %%% Plot normal fault results.
-nexttile
+nexttile(layout1)
 hold on;
 box on;
-for i = 1:numel(Burial_Star)
+for i = 1:numel(Burial_hat)
     if i == 1
         plot(beta(2:end), cell2mat(T_N06.(i)), col{i}, 'LineWidth', 3);
     else
@@ -147,7 +155,7 @@ for i = 1:numel(Burial_Star)
     end
 end
 
-for i = 1:numel(Burial_Star(1:4))
+for i = 1:numel(Burial_hat(1:4))
     if i == 1
         m02 = plot(beta(2:end), cell2mat(T_N02.(i)), col{i}, 'LineWidth',...
             3, 'LineStyle', '--');
@@ -173,7 +181,7 @@ ylabel('Critical Fault Length ($L^*_D / h_F^*$)', 'Interpreter', 'latex')
 xlim([0 90])
 ylim([0 0.4])
 ax = gca;
-ax.FontSize = 20;
+ax.FontSize = 22;
 ax.XTick = 0:10:90;
 ax.YTick = [(0:0.05:0.35) exp(-1) 0.4];
 ax.YTickLabel{9,1} = '$e^{-1}$';
@@ -200,47 +208,47 @@ title(lgd2, '$\mu_0$', 'Interpreter', 'latex')
 [~, i_20] = min(abs(beta - 20));
 
 figure;
-tiledlayout(1,2);
-nexttile;
+set(gcf,'Color','w')
+layout2 = tiledlayout(1, 2, 'Padding', 'tight');
+nexttile(layout2)
 
 %%% Thrust faults.
 ct = [217 83 25]/255;
-t1 = plot(Burial_Star, cell2mat(T_T06{i_05,:})', ':o', 'Color', ct,...
+t1 = plot(Burial_hat, cell2mat(T_T06{i_05,:})', ':o', 'Color', ct,...
     'MarkerEdgeColor', 'k', 'MarkerFaceColor', ct, 'MarkerSize', 10,...
     'LineWidth', 3);
-drawnow
-t1.MarkerHandle.LineWidth = 1;
+% drawnow
+% t1.MarkerHandle.LineWidth = 1;
 hold on
-t2 = plot(Burial_Star, cell2mat(T_T06{i_10,:})', '-o', 'Color', ct,...
+t2 = plot(Burial_hat, cell2mat(T_T06{i_10,:})', '-o', 'Color', ct,...
     'MarkerEdgeColor', 'k', 'MarkerFaceColor', ct, 'MarkerSize', 10,...
     'LineWidth', 3);
-drawnow
-t2.MarkerHandle.LineWidth = 1;
-t3 = plot(Burial_Star, cell2mat(T_T06{i_20,:})', '--o', 'Color', ct,...
+% drawnow
+% t2.MarkerHandle.LineWidth = 1;
+t3 = plot(Burial_hat, cell2mat(T_T06{i_20,:})', '--o', 'Color', ct,...
     'MarkerEdgeColor', 'k', 'MarkerFaceColor', ct, 'MarkerSize', 10,...
     'LineWidth', 3);
-drawnow
-t3.MarkerHandle.LineWidth = 1;
+% drawnow
+% t3.MarkerHandle.LineWidth = 1;
 
-% t2b = plot(Burial_Star, cell2mat(T_T06{i_10,:})', '-', 'Color', ct, 'LineWidth', 3);
 
 %%% Normal faults.
 cn = [114 189 255]/255;
-n1 = plot(Burial_Star, cell2mat(T_N06{i_05,:})', ':s', 'Color', cn,...
+n1 = plot(Burial_hat, cell2mat(T_N06{i_05,:})', ':s', 'Color', cn,...
     'MarkerEdgeColor', 'k', 'MarkerFaceColor', cn, 'MarkerSize', 10,...
     'LineWidth', 3);
-drawnow
-n1.MarkerHandle.LineWidth = 1;
-n2 = plot(Burial_Star, cell2mat(T_N06{i_10,:})', '-s', 'Color', cn,...
+% drawnow
+% n1.MarkerHandle.LineWidth = 1;
+n2 = plot(Burial_hat, cell2mat(T_N06{i_10,:})', '-s', 'Color', cn,...
     'MarkerEdgeColor', 'k',    'MarkerFaceColor', cn, 'MarkerSize', 10,...
     'LineWidth', 3);
-drawnow
-n2.MarkerHandle.LineWidth = 1;
-n3 = plot(Burial_Star, cell2mat(T_N06{i_20,:})', '--s', 'Color', cn,...
+% drawnow
+% n2.MarkerHandle.LineWidth = 1;
+n3 = plot(Burial_hat, cell2mat(T_N06{i_20,:})', '--s', 'Color', cn,...
     'MarkerEdgeColor', 'k', 'MarkerFaceColor', cn, 'MarkerSize', 10,...
     'LineWidth', 3);
-drawnow
-n3.MarkerHandle.LineWidth = 1;
+% drawnow
+% n3.MarkerHandle.LineWidth = 1;
 
 %%% exp(-1) line.
 plot([1e-3 1], exp(-1)*[1 1], 'k', 'LineWidth', 3)
@@ -259,7 +267,7 @@ ax.TickLabelInterpreter = 'latex';
 xlabel('Normalized Burial Depth ($d / h^*_F$)', 'Interpreter', 'latex')
 ylabel('Critical Fault Length ($L^*_D / h_F^*$)', 'Interpreter', 'latex')
 
-text(nexttile(1), 0.05, 0.05, 'A', 'units', 'normalized', 'FontSize', 30,...
+text(0.05, 0.05, 'A', 'units', 'normalized', 'FontSize', 30,...
     'Interpreter', 'latex')
 
 lgd = legend('0.5', '10', '20', '0.5', '10', '20', 'Location', 'southeast',...
@@ -267,12 +275,20 @@ lgd = legend('0.5', '10', '20', '0.5', '10', '20', 'Location', 'southeast',...
 lgd.NumColumns = 2;
 title(lgd, '$\beta$ (deg)', 'Interpreter', 'latex')
 
+drawnow
+t1.MarkerHandle.LineWidth = 1;
+t2.MarkerHandle.LineWidth = 1;
+t3.MarkerHandle.LineWidth = 1;
+n1.MarkerHandle.LineWidth = 1;
+n2.MarkerHandle.LineWidth = 1;
+n3.MarkerHandle.LineWidth = 1;
+
 % ax2 = axes('position', ax.Position, 'visible', 'off');
 % lgd2 = legend(ax2, [t2b n2], 'thrust', 'normal', '1', 'Location', 'south',...
 %     'Interpreter', 'latex');
 % lgd2.FontSize = 18;
 
-drawnow
+% drawnow
 
 lineEntry = findobj(lgd.EntryContainer, 'Object',t1);
 entryMarker = findobj(lineEntry.Icon.Transform, 'Description','Icon Marker');
@@ -309,7 +325,7 @@ entryMarker.Style = 'none';
 [Beta, Shear, G_thrust, G_normal] = StressingRates;
 
 %%% Plot the results.
-nexttile
+nexttile(layout2)
 plot(Beta, abs(Shear), 'k', 'LineWidth', 3);
 hold on;
 
@@ -338,7 +354,7 @@ ax.FontSize = 20;
 ax.XTick = 0:10:90;    
 ax.TickLabelInterpreter = 'latex';
 
-text(nexttile(2), 0.05, 0.05, 'B', 'units', 'normalized', 'FontSize', 30,...
+text(0.05, 0.05, 'B', 'units', 'normalized', 'FontSize', 30,...
     'Interpreter', 'latex')
 
 lgdB = legend('0', '0.2', '0.6', '1', '0.2', '0.6', '1', 'Location', 'southeast',...
